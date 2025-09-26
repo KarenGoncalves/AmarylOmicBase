4938e740d089fe71047b811f953c5a611cfb1753
KarenGoncalves <cris.kgs@gmail.com>
2025-09-26 06:27:48 -0700

    predict pep and load initiate DB

---
+++ 08-transdecoderPredict.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Requieres transdecoder trinotate/4.0
+
+SPECIES=
+SPECIES_DIR=
+ASSEMBLY=
+TrinotatePFAM=
+blastp6_out=
+
+source ~/trinotateAnnotation/bin/activate
+
+TransDecoder.Predict\
+ -t $ASSEMBLY\
+ --output_dir $SPECIES_DIR\
+ --retain_pfam_hits ${TrinotatePFAM}\
+ --retain_blastp_hits ${blast6_out}\
+ --single_best_only
+
+$TRINOTATE_HOME/util/trinotateSeqLoader/TrinotateSeqLoader.pl\
+ --sqlite $SPECIES_DIR/Trinotate.sqlite\
+ --gene_trans_map $SPECIES_DIR/${SPECIES}_gene_trans_map\
+ --transcript_fasta ${ASSEMBLY}\
+ --transdecoder_pep ${ASSEMBLY}.transdecoder.pep\
+ --bulk_load
