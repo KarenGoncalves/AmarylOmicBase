7f8feb4a9f0c680501dc580821698158fb299e19
Karen Cristine Gonçalves <cris.kgs@gmail.com>
2026-05-05 16:12:41 -0400

    Update 01-sra_dump.sh
    correcting seq-defline and adding prefetch to speed up download

--- 01-sra_dump.sh
+++ 01-sra_dump.sh
@@ -6,11 +6,17 @@
 
 SPECIES_DIR=
 cd $SPECIES_DIR/
-
 cat metadata/SRA_accessions.txt |
  while read ACCESSION; do
-	 fasterq-dump\
-	 --seq-defline '@$sn[_$rn]/$ri'\
-	 --split-files ${ACCESSION}\
-	 --outdir RAW_DATA/ 
+        if [[  -e RAW_DATA/${ACCESSION}.fastq || -e RAW_DATA/${ACCESSION}_1.fastq ]]; then  continue;
+        else
+                if [[ ! -e ${ACCESSION}/$ACCESSION.sra ]]; then
+                        prefetch  $ACCESSION
+                else
+                        echo "Prefetch done for $ACESSION, downloading fastq"
+                fi
+                fasterq-dump --seq-defline "@\$ac.\$si/\$ri"\
+                 --split-files ${ACCESSION}/$ACCESSION.sra\
+                 --outdir RAW_DATA/
+        fi
 done
